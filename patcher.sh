8a53ea01b78afd137f0a80aa26da13a5bc4394b0
Suggon <maharjansagun99@gmail.com>
2026-02-09 22:03:21 +0545

    add PREFIX env and make usable from any PWD

--- patcher.sh
+++ patcher.sh
@@ -2,14 +2,14 @@
 set -e
 
 if [ ! $(whoami) = "root" ]; then
-	echo "###\033[96m Soul Knight Patcher \033[0m###"
+	echo -e "###\033[96m Soul Knight Patcher \033[0m###"
 	echo "Modifies user data to unlock various contents."
 	echo
 	echo 'This script needs to be run as root to access `/data`.'
 	exit 1
 fi
 
-xml_file="/data/data/com.ChillyRoom.DungeonShooter/shared_prefs/com.ChillyRoom.DungeonShooter.v2.playerprefs.xml"
+xml_file="$PREFIX/data/data/com.ChillyRoom.DungeonShooter/shared_prefs/com.ChillyRoom.DungeonShooter.v2.playerprefs.xml"
 
 if [ -e "$xml_file" ]; then
 	echo "Patching file: $xml_file"
@@ -19,15 +19,16 @@ else
 	exit 1
 fi
 
-sed -i.old -E -f ./substitutions.sed $xml_file
+script_dir="$(dirname $0)"
+sed -i.old -E -f $script_dir/substitutions.sed $xml_file
 
 rundiff() {
 	! diff --color -s -U0 $xml_file.old $xml_file
 }
 
 rundiff
-rundiff > ./patch.diff
+rundiff > $script_dir/patch.diff
 
 echo
 echo "Saved backup xml: $xml_file.old"
-echo "Saved diff file: ./patch.diff"
+echo "Saved diff file: $script_dir/patch.diff"
